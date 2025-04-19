27321807348775d14848ab096edf7ab501da63b4
voowu <kupisoft22@gmail.com>
2025-04-19 23:50:52 +0200

    addSwiftCode endpoint
    - addSwiftCode endpoint
    - InvalidDataException
    - ResourceAlreadyExistsException
    - changes: repository, apiService, handler, controller

--- src/main/java/com/excercises/swiftAPI/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/excercises/swiftAPI/exceptions/GlobalExceptionHandler.java
@@ -22,6 +22,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    public ResponseEntity<String> invalidDataException(InvalidDataException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception e) {
         return new ResponseEntity<>("Unexpected error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
